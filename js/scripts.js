51d34774bd9aa5294e47d43b5b7c4cdf927a189a
RGB Studios <justingolden21@gmail.com>
2021-03-09 16:35:22 -0800

    we got the sets

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-window.onload = ()=> {
+$(()=> {
 	console.log('page loaded');
-};
+
+	$.getJSON('https://api.scryfall.com/sets', function(res) {
+		let sets = res.data;
+		let html = '';
+		for(let set of sets) {
+			html += `<div class="set" title="${set.name}" onclick="openSet('${set.search_uri}')">
+				<img src="${set.icon_svg_uri}" width="32px">
+				<span class="set-code">${set.code.toUpperCase()}</span>
+				<span class="set-name">${set.name}</span>
+			</div>`;
+		}
+		$('#sets').append(html);
+	});
+
+});
+
+function openSet(uri) {
+	console.log(uri);
+}
